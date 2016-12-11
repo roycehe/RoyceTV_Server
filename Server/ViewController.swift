e83d1dd92143593b165f20487654ba43087301ad
小码哥 <xiaomage@xiaomagedeimac.local>
2016-12-11 14:50:20 +0800

    Initial Commit

---
+++ Server/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by 小码哥 on 2016/12/11.
+//  Copyright © 2016年 xmg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
